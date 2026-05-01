6416ecc4ee5394efce23fbe8b89b208415e779a6
zuneira <zunairasayed20@gmail.com>
2026-05-01 11:48:59 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>TODO LIST</h1>
+      <form>
+        <input type="text"/> <button>Save</button>
+      </form>
+    </div>
+
+  );
+}
+
+export default App;
